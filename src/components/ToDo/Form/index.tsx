5f555a5ced5620b6b36c3a5dce2b102ab342adb3
Renato Martinez Costa <renatomc@hotmail.com>
2023-11-02 01:59:38 -0300

    Criando eventos no form

--- src/components/ToDo/Form/index.tsx
+++ src/components/ToDo/Form/index.tsx
@@ -1,17 +1,25 @@
-import { FormHTMLAttributes } from 'react';
+import { ChangeEvent, Dispatch, FormHTMLAttributes, SetStateAction } from 'react';
 
 import { Input } from '../../Form/Input';
 import { Button } from '../../Form/Button';
 
 import styles from './styles.module.css';
 
-interface FormProps extends FormHTMLAttributes<HTMLFormElement> {}
+interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
+  todo: string;
+  setTodo: Dispatch<SetStateAction<string>>;
+}
 
-export function Form({...rest}: FormProps) {
+export function Form({todo, setTodo, ...rest}: FormProps) {
+
+  function handleInputTodo(event: ChangeEvent<HTMLInputElement>) {
+    setTodo(event?.target.value);
+  }
+  
   return (
     <div className={styles.toDoForm}>
       <form {...rest}>
-        <Input placeholder='Selecione uma nova tarefa' />
+        <Input placeholder='Selecione uma nova tarefa' value={todo} onChange={handleInputTodo} />
         <Button type='submit'>Criar</Button>
       </form>
     </div>
--- src/components/ToDo/index.tsx
+++ src/components/ToDo/index.tsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import { Form } from './Form';
 import styles from './styles.module.css';
 
 export function ToDo() {
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState(['']);
+
+  function handleSubmit() {
+    setTodoList([...todoList, todo]);
+    setTodo('');
+  }
+
+  console.log({todoList});
+  
   return (
     <div className={styles.todo}>
-      <Form />
+      <Form onSubmit={handleSubmit} todo={todo} setTodo={setTodo}  />
     </div>
   );
 }
